680d2f79f982c1bdb9621065d0b9b61db0238fa3
Wasim Abuzaher <wasim.abuzaher@gmail.com>
2023-10-09 22:20:16 -0400

    [Exercise 4]: Add Game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ guess }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}`:  "cell";
+  return (
+      <span className={className}>{letter}</span>
+  )
+}
+
+function Guess({ guess, answer }) {
+  const checkedWord = checkGuess(guess?.guessWord, answer);
+
   return <p className="guess">
     {range(0, 5).map((v) => (
-        <span key={`${guess?.id}-${crypto.randomUUID()}`} className="cell">{guess ? guess.guessWord[v] : ''}</span>
+      <Cell
+          key={`${guess?.id}-${crypto.randomUUID()}`}
+          letter={checkedWord ? checkedWord[v].letter : undefined}
+          status={checkedWord ? checkedWord[v].status : undefined}
+      />
     ))}
   </p>;
 }
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,10 +3,10 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "../Guess";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return <div className="guess-results">
     {range(0, NUM_OF_GUESSES_ALLOWED).map((v) => (
-        <Guess key={v} guess={guesses[v]} />
+        <Guess key={v} guess={guesses[v]} answer={answer} />
     ))}
   </div>
 }
